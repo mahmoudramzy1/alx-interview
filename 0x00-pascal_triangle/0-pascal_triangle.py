e154f2d48eb4626c7176331f74f911fef6ab2db5
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-10-06 17:22:43 +0300

    p

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """making pascal triangle"""
 
+
 def pascal_triangle(n):
     """function that makes pascal triangle with n of arrays """
     big = []
-    
+
     if n <= 0:
         return []
     else:
         big = [[1]]
-        
+
         if n >= 2:
             for i in range(1, n):
                 row = [1]
-                for j in range (1, i):
+                for j in range(1, i):
                     row.append(big[i-1][j-1] + big[i-1][j])
                 row.append(1)
                 big.append(row)
